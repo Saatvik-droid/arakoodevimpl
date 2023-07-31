b2c7eda9b414e19e8cf23f217aa7ac24c2d035c6
Saatvik Ramani <saatvikramani777@gmail.com>
2023-08-01 00:55:59 +0530

    [add] hyde prompt templates

--- HyDE/HyDE.py
+++ HyDE/HyDE.py
@@ -2,37 +2,22 @@ import os
 
 import numpy as np
 import openai
-import pinecone
-from dotenv import load_dotenv
 import redis
+from dotenv import load_dotenv
 from redis.commands.search.field import TagField, VectorField
 from redis.commands.search.indexDefinition import IndexDefinition, IndexType
 from redis.commands.search.query import Query
 
-load_dotenv()
+from prompt import PromptGen
 
-query = "how long does it take to remove wisdom tooth"
+load_dotenv()
+prompt_gen = PromptGen()
 openai.api_key = os.getenv("OPENAI_API_KEY")
 openai.organization = os.getenv("OPENAI_ORG")
 
-# pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment="northamerica-northeast1-gcp")
-# index = pinecone.Index("hyde")
-
-
-result = openai.Completion.create(
-    engine="text-davinci-003",
-    prompt=query,
-    temperature=0.7,
-    n=8,
-    max_tokens=512,
-    top_p=1,
-    stop=['\n\n\n'],
-    logprobs=1
-)
-
 r = redis.Redis(
-    host='redis-12487.c264.ap-south-1-1.ec2.cloud.redislabs.com',
-    port=12487,
+    host='redis-18848.c212.ap-south-1-1.ec2.cloud.redislabs.com',
+    port=18848,
     password=os.getenv("REDIS_PASSWORD"))
 
 INDEX_NAME = "hyde"  # Vector Index Name
@@ -99,20 +84,19 @@ def embed(document):
     return embeddings
 
 
-samples = [
-    "However, if you do feel pain during the procedure, tell your dentist or oral surgeon so they can give you more anaesthetic. How long it takes to remove the tooth will vary. Simple procedures can take a few ""minutes, but it can take longer than 20 minutes if it's more complicated.",
-    "Your surgery should take 45 minutes or less. You'll get one of these types of anesthesia so you don't feel pain during the removal: Local: Your doctor will numb your mouth with a shot of local anesthetic such as novocaine, lidocaine or mepivicaine.",
-    "The time it takes to remove the tooth will vary. Some procedures only take a few minutes, whereas others can take 20 minutes or longer. After your wisdom teeth have been removed, you may experience swelling and discomfort, both on the inside and outside of your mouth. This is usually worse for the first three days, but it can last for up to two weeks. Read more about how a wisdom tooth is removed and recovering from wisdom tooth removal.",
-    "How long does it take to remove all wisdom teeth?   I got my wisdom teeth removed 5 days ago. I received intravenous anesthesia, so I was not conscious during the process, but those present said it only took about 35 to 40â€¦ minutes for removal."
-    "Complications like infection can lengthen the time it takes to heal up, but here is a general timeline: 1  Swelling and pain will be the greatest during the first 3 days (peaking at about 48hours). 2  Normally, the sockets should take about 2 weeks to 1 month to cover over with solid gum tissue after scabbing first.",
-    "How wisdom teeth are removed. Your dentist may remove your wisdom teeth or they may refer you to a specialist surgeon for hospital treatment. Before the procedure, you'll usually be given a local anaesthetic injection to numb the area around the tooth.he time it takes to remove the tooth will vary. Some procedures only take a few minutes, whereas others can take 20 minutes or longer. After your wisdom teeth have been removed, you may experience swelling and discomfort, both on the inside and outside of your mouth."
-]
-
-# Inserted once and hence commented out
-
 create_index(1536)
-for sample in samples:
-    add_docs(sample)
+
+query = prompt_gen.WEB_SEARCH.format("how long does it take to remove wisdom tooth")
+result = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=query,
+    temperature=0.7,
+    n=8,
+    max_tokens=512,
+    top_p=1,
+    stop=['\n\n\n'],
+    logprobs=1
+)
 
 # logarithmic probability sorting
 to_return = []
@@ -122,19 +106,22 @@ for _, val in enumerate(result['choices']):
     to_return.append((text, logprob))
 texts = [r[0] for r in sorted(to_return, key=lambda tup: tup[1], reverse=True)]
 
+for doc in texts:
+    add_docs(doc)
+
 print(">Generated responses:")
 text_embeddings = []
 for idx, doc in enumerate(texts):
     print(doc.strip())
     embedding = embed(doc.strip())
     text_embeddings.append(embedding)
+
 # merging response embeddings
 text_embeddings = np.array(text_embeddings)
 mean_embedding = np.mean(text_embeddings, axis=0)
 result_vector = mean_embedding.tobytes()
-# retrieving k most similar documents
-# query_response = index.query(top_k=2, include_metadata=True, vector=result_vector[0].tolist())
+
 query_response = retrieve(result_vector, 2)
-print(">Retrieved documents")
+print(">Retrieved documents:")
 for res in query_response:
-    print(res.content)
+    print(res.content.strip())
---
+++ HyDE/prompt.py
@@ -0,0 +1,32 @@
+class PromptGen:
+    WEB_SEARCH = """Please write a passage to answer the question.
+    Question: {}
+    Passage:"""
+
+    SCIFACT = """Please write a scientific paper passage to support/refute the claim.
+    Claim: {}
+    Passage:"""
+
+    ARGUANA = """Please write a counter argument for the passage.
+    Passage: {}
+    Counter Argument:"""
+
+    TREC_COVID = """Please write a scientific paper passage to answer the question.
+    Question: {}
+    Passage:"""
+
+    FIQA = """Please write a financial article passage to answer the question.
+    Question: {}
+    Passage:"""
+
+    DBPEDIA_ENTITY = """Please write a passage to answer the question.
+    Question: {}
+    Passage:"""
+
+    TREC_NEWS = """Please write a news passage about the topic.
+    Topic: {}
+    Passage:"""
+
+    MR_TYDI = """Please write a passage in {} to answer the question in detail.
+    Question: {}
+    Passage:"""
